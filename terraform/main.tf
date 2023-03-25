8c139611aa1cf2030633580ee6b15784b52a36c5
kesompochy <kesompochy@gmail.com>
2023-03-26 04:12:58 +0900

    create cluster

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+provider "google" {
+  project = "avid-influence-381703"
+}
+
+resource "google_container_cluster" "cluster" {
+  name               = "kesompochy-page"
+  location           = "us-central1-a"
+  initial_node_count = 3
+
+  node_config {
+    machine_type = "n1-standard-2"
+  }
+
+  master_auth {
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
